fa1d11fed1ec211cd226ffe6205c77aed5ab0d32
Adrian-Batista <62628581+Adrian-Batista@users.noreply.github.com>
2020-09-24 21:55:00 -0300

    Update

--- Servidor/src/Entidades/Vetor.java
+++ Servidor/src/Entidades/Vetor.java
@@ -1,5 +1,95 @@
 package Entidades;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Vetor {
+	
+	private static Socket s;
+	
+	public static void OrdenaVetor(ServerSocket ss) throws IOException { // ~~~~~~~~~~~~~~~~~~~~~~ MÉTODO VETOR ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		
+		int[] vetor = new int[250000];
+		BufferedReader entrada;
+		DataOutputStream saida;
+		String dadoCliente = null;
+		
+		for(int auxiliar = 0; auxiliar<250000; auxiliar++) { // ~~~~~~~~~~~~ carregando o vetor com os dados recebidos da conexão. 
+			s = ss.accept();
+			entrada = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			dadoCliente = entrada.readLine();
+			vetor[auxiliar] = Integer.parseInt(dadoCliente);
+			System.out.println("\nValor: "+ vetor[auxiliar]);
+			saida = new DataOutputStream(s.getOutputStream());
+			saida.writeBytes("recebido!");
+			saida.flush();
+			entrada.close();
+			saida.close();
+		}
+		
+		 long tempoInicial = System.currentTimeMillis();
+
+		 insertionSortVetor(vetor);
+
+	     long tempoFinal = System.currentTimeMillis();
+
+	     System.out.println("Executado em = " + (tempoFinal - tempoInicial) + " ms");
+	     
+	     System.in.read();
+	     
+	     for(int i = 0; i<vetor.length; i++) {
+	    	 System.out.println("Valor: "+ vetor[i]);
+	     }
+		
+	}
+	
+	public static void insertionSortVetor(int[] vetor) {
+	    int j;
+	    int key;
+	    int i;
+
+	    for (j = 1; j < vetor.length; j++)
+	    {
+	      key = vetor[j];
+	      for (i = j - 1; (i >= 0) && (vetor[i] > key); i--)
+	      {
+	         vetor[i + 1] = vetor[i];
+	       }
+	        vetor[i + 1] = key;
+	    }
+	}
+	
+	 private static void quickSortVetor(int[] vetor, int inicio, int fim) {
+         if (inicio < fim) {
+                int posicaoPivo = separarVetor(vetor, inicio, fim);
+                quickSortVetor(vetor, inicio, posicaoPivo - 1);
+                quickSortVetor(vetor, posicaoPivo + 1, fim);
+         }
+   }
+
+   private static int separarVetor(int[] vetor, int inicio, int fim) {
+         int pivo = vetor[inicio];
+         int i = inicio + 1, f = fim;
+         while (i <= f) {
+                if (vetor[i] <= pivo)
+                       i++;
+                else if (pivo < vetor[f])
+                       f--;
+                else {
+                       int troca = vetor[i];
+                       vetor[i] = vetor[f];
+                       vetor[f] = troca;
+                       i++;
+                       f--;
+                }
+         }
+         vetor[inicio] = vetor[f];
+         vetor[f] = pivo;
+         return f;
+   }
 
 }
--- Servidor/src/Principal/Main.java
+++ Servidor/src/Principal/Main.java
@@ -7,6 +7,8 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import Entidades.Vetor;
+
 public class Main {
 
 	private static Socket s;
@@ -31,7 +33,7 @@ public class Main {
 			saida.close();
 			
 			if(dadoCliente.contains("vetor")) {
-				OrdenaVetor();
+				Vetor.OrdenaVetor(ss);
 			}
 			if(dadoCliente=="encadeada") {
 				
@@ -46,40 +48,7 @@ public class Main {
 	
 	}
 	
-	public static void OrdenaVetor() throws IOException { // ~~~~~~~~~~~~~~~~~~~~~~ MÉTODO VETOR ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-		int[] vetor = new int[250000];
-		BufferedReader entrada;
-		DataOutputStream saida;
-		String dadoCliente = null;
-		
-		for(int auxiliar = 0; auxiliar<250000; auxiliar++) { // ~~~~~~~~~~~~ carregando o vetor com os dados recebidos da conexão. 
-			s = ss.accept();
-			entrada = new BufferedReader(new InputStreamReader(s.getInputStream()));
-			dadoCliente = entrada.readLine();
-			vetor[auxiliar] = Integer.parseInt(dadoCliente);
-			System.out.println("\nValor: "+ vetor[auxiliar]);
-			saida = new DataOutputStream(s.getOutputStream());
-			saida.writeBytes("recebido!");
-			saida.flush();
-			entrada.close();
-			saida.close();
-		}
-		
-		 long tempoInicial = System.currentTimeMillis();
-
-	     insertionSortVetor(vetor);
-
-	     long tempoFinal = System.currentTimeMillis();
-
-	     System.out.println("Executado em = " + (tempoFinal - tempoInicial) + " ms");
-	     
-	     System.in.read();
-	     
-	     for(int i = 0; i<vetor.length; i++) {
-	    	 System.out.println("Valor: "+ vetor[i]);
-	     }
-		
-	}
+	
 	
 	public static void OrdenaEncadeada() { // ~~~~~~~~~~~~~~~~~~~~~~ MÉTODO ENCADEADA ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 		
@@ -89,49 +58,6 @@ public class Main {
 	
 	}
 	
-	public static void insertionSortVetor(int[] vetor) {
-	    int j;
-	    int key;
-	    int i;
-
-	    for (j = 1; j < vetor.length; j++)
-	    {
-	      key = vetor[j];
-	      for (i = j - 1; (i >= 0) && (vetor[i] > key); i--)
-	      {
-	         vetor[i + 1] = vetor[i];
-	       }
-	        vetor[i + 1] = key;
-	    }
-	}
 	
-	 private static void quickSortVetor(int[] vetor, int inicio, int fim) {
-         if (inicio < fim) {
-                int posicaoPivo = separarVetor(vetor, inicio, fim);
-                quickSortVetor(vetor, inicio, posicaoPivo - 1);
-                quickSortVetor(vetor, posicaoPivo + 1, fim);
-         }
-   }
-
-   private static int separarVetor(int[] vetor, int inicio, int fim) {
-         int pivo = vetor[inicio];
-         int i = inicio + 1, f = fim;
-         while (i <= f) {
-                if (vetor[i] <= pivo)
-                       i++;
-                else if (pivo < vetor[f])
-                       f--;
-                else {
-                       int troca = vetor[i];
-                       vetor[i] = vetor[f];
-                       vetor[f] = troca;
-                       i++;
-                       f--;
-                }
-         }
-         vetor[inicio] = vetor[f];
-         vetor[f] = pivo;
-         return f;
-   }
 
 }
